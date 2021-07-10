4e46e55b1c3166eaec18d9217852519d7eb2c700
Bruna Nunes <bruna.f.n@hotmail.com>
2021-07-10 13:24:01 -0300

    configurando webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// cada sistema operacional usa a barra de uma forma e o path vai fazer esse papel de saber qual barra usar no caminhos
+const path = require ('path');
+
+module.exports = {
+    // aqui o arquivo principal da aplicação
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    // qual arquivo será gerado
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    // por padrão ele lê arquivos js, adicionamos jsx também
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    // como a  aplicação vai ser comportar com esses arquivos
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modulues/,
+                use: 'babel-loader'
+            }
+        ],
+    }
+};
