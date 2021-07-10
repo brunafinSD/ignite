9273266cffdbc739914013616b70c4939c1d8804
Bruna Nunes <bruna.f.n@hotmail.com>
2021-07-10 14:59:45 -0300

    ambiente dev e produção

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@
 const path = require ('path');
 const HtmlWebpackPlugin = require ('html-webpack-plugin');
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     // aqui o arquivo principal da aplicação
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     // qual arquivo será gerado
